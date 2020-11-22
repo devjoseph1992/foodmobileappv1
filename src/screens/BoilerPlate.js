709c8906fe9a649b8e4f8f43b04c7e866dd2cc14
john joseph florina <devjosephflorina@gmail.com>
2020-11-22 14:19:41 +0800

    fix errors

---
+++ src/screens/BoilerPlate.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return <Text>Search Plate</Text>;
+};
+
+const Styles = StyleSheet.create({});
+
+export default SearchScreen;
